07891fa4625900859e48f7d34762e1109d1e4c11
adeoyewole028 <adeoyewole028@gmail.com>
2022-06-02 22:04:54 +0100

    test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let splitting = string.split("");
+  let reverse = splitting.reverse();
+  let joining = reverse.join("");
+  let str = joining.toString();
+  return str;
 };
 
 // Do not edit below this line
