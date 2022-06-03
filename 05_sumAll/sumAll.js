76843f513c55f63fbec7fd20b90ef331690ffda1
adeoyewole028 <adeoyewole028@gmail.com>
2022-06-03 21:08:11 +0100

    test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (num2 < typeof String || num1 < 0) {
+    return "ERROR";
+  } else if (num1 > 1) {
+    for (let i = 0; i <= num2; i++) {
+      let sum = (num1 * (num1 + num2)) / 2;
+      return sum;
+    }
+  }
+  for (let i = 0; i <= num1; i++) {
+    let sum = (num2 * (num2 + num1)) / 2;
+    return sum;
+  }
 };
 
 // Do not edit below this line
