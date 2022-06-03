adb1bc23e1ffcaaaa960179fa519aa55bc2700e3
adeoyewole028 <adeoyewole028@gmail.com>
2022-06-03 19:54:45 +0100

    test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (...arr) {
+  let array = arr[0];
+  return array.filter((value) => !arr.includes(value));
 };
 
 // Do not edit below this line
