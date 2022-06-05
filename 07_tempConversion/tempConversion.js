5a545b481a49aa4a98bacb2c87e2612610d83bea
adeoyewole028 <adeoyewole028@gmail.com>
2022-06-05 20:29:40 +0100

    test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fah) {
+  let cel = (fah - 32) * (5 / 9);
+  return Math.round(cel * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (cel) {
+  let fah = cel * (9 / 5) + 32;
+  return Math.round(fah * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
