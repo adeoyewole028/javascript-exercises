a40c59041817759daa1bd9762415592382e277c7
adeoyewole028 <adeoyewole028@gmail.com>
2022-08-18 23:04:29 +0100

    completed challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(int) {
+  if (int < 0) {
+    return "OOPS";
+  } else if (int === 0) {
+    return 0;
+  } else if (int == 1) {
+    return 1;
+  } else {
+    return fibonacci(int - 1) + fibonacci(int - 2);
+  }
 };
 
 // Do not edit below this line
