a97fffdc24ff15e1597b35446a60fe6b9d673177
adeoyewole028 <adeoyewole028@gmail.com>
2022-08-18 22:57:12 +0100

    challenge completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // set the string to lowercase and use regExp to remove unwanted characters
+  let removeWithReg = /[\W_]/g;
+  let low = string.toLowerCase().replace(removeWithReg, "");
+  // set the string to an array of characters reverse and join them back together to create a new string
+  let rev = low.split("").reverse().join("");
+  // if the new string is the same as the original string, return true, else return false
+  return low === rev;
 };
 
 // Do not edit below this line
