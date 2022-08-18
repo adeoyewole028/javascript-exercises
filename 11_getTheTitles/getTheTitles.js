1e836a5f9e2b68125d84a3c3c8de1e71dcd921c6
adeoyewole028 <adeoyewole028@gmail.com>
2022-08-18 23:08:38 +0100

    challenge completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+  return array.map(function (object) {
+    return object.title;
+  });
 };
 
 // Do not edit below this line
